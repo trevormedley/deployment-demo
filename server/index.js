d692b0c50966c0bfff010542632673053fec6680
trevormedley <trevormedley10@gmail.com>
2021-11-17 12:03:56 -0500

    Update 1

--- server/index.js
+++ server/index.js
@@ -4,8 +4,10 @@ const app = express();
 
 const port = process.env.PORT || process.env.SERVER_PORT;
 
+app.use(express.static("client"));
 
-
-
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+})
 
 app.listen(port, () => console.log(`We are running on port: ${port}`));
